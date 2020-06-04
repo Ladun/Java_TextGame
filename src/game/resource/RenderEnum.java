1742c34409e6bd4ac4b9e05b69d0c13fcb85c92c
Ladun <ladun9128@gmail.com>
2020-06-04 14:31:19 +0900

    Frame 틀 만든느 함수 생성

--- src/game/resource/RenderEnum.java
+++ src/game/resource/RenderEnum.java
@@ -5,5 +5,8 @@ public enum RenderEnum {
 	TAG_START_MAIN, 
 	TAG_START_SELECT,
 	TAG_SHOP,
-	TAG_MAIN
+	TAG_MAIN,
+	TAG_TAVERN,
+	MAGICIAN,
+	NONE
 }
--- src/game/screen/TavernScene.java
+++ src/game/screen/TavernScene.java
@@ -1,13 +1,20 @@
 package game.screen;
 
 import game.GameManager;
+import game.resource.RenderEnum;
+import game.resource.Resources;
 
 public class TavernScene extends AbstractScene{
 
 	@Override
 	public void show(GameManager gm) {
-		// TODO Auto-generated method stub
 		
+		Resources.print(3, 2, 
+				RenderEnum.MAGICIAN, RenderEnum.NONE, RenderEnum.NONE,
+			    RenderEnum.MAGICIAN, RenderEnum.NONE, RenderEnum.NONE);
+		
+		
+		int in = gm.getInput().getInt(1, 3);
 	}
 
 }
